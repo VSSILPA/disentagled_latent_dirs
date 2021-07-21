33795d7ac1bfeb254c46319aa34bd4643583e6f8
adarsh <kadarsh22@gmail.com>
2021-07-21 11:25:26 +0530

    saver_corrected

--- src/saver.py
+++ src/saver.py
@@ -10,7 +10,7 @@ class Saver(object):
         self.experiment_name = self.config['experiment_name']
         self.model_name = self.config['file_name']
 
-    def save_model(self, params, seed_idx, step, generator_idx, algo='LD'):
+    def save_model(self, params, step,generator_idx,algo='LD'):
         cwd = os.path.dirname(os.getcwd()) + f'/results/{self.experiment_name}'  # project root
         models_dir = cwd + '/models/'
 
@@ -29,30 +29,26 @@ class Saver(object):
                 'random_state': random.getstate()
 
             }, os.path.join(models_dir, str(step) + str(generator_idx) + '_model.pkl'))
-        elif algo == 'ours':
-                deformator, deformator_opt, cr_discriminator, cr_optimizer = params
-                dict_ = {
-                    'seed': seed_idx,
-                    'generator_number': generator_idx,
+        elif algo == 'ours-natural':
+                deformator, cr_discriminator, deformator_opt, cr_optimizer = params
+                torch.save({
                     'step': step,
                     'deformator': deformator.state_dict(),
-                    'deformator_opt': deformator_opt.state_dict(),
                     'cr_discriminator': cr_discriminator.state_dict(),
-                    'cr_optimizer' : cr_optimizer.state_dict(),
+                    'deformator_opt': deformator_opt.state_dict(),
+                    'cr_optimizer': cr_optimizer.state_dict(),
                     'torch_rng_state': torch.get_rng_state(),
                     'np_rng_state': np.random.get_state(),
                     'random_state': random.getstate()
 
-                }
-                torch.save(dict_, os.path.join(models_dir, str(seed_idx) + '_' + str(generator_idx) + '_model.pkl'))
-                torch.save(dict_, os.path.join(models_dir, 'checkpoint_latest.pkl'))
+                }, os.path.join(models_dir, str(step) + str(generator_idx) + '_model.pkl'))
         else:
             raise NotImplementedError
 
     def load_model(self, params, algo='LD'):
-        cwd = os.path.dirname(os.getcwd()) + f'/results/{self.experiment_name}'  # project root
-        models_dir = cwd + '/models/checkpoint_latest.pkl'
+        models_dir = os.path.dirname(os.getcwd()) + f'/pretrained_models/' + self.config['file_name']  # project root
         checkpoint = torch.load(models_dir)
+        print(algo)
         if algo == 'LD':
             deformator, shift_predictor, deformator_opt, shift_predictor_opt = params
             deformator.load_state_dict(checkpoint['deformator'])
@@ -64,25 +60,17 @@ class Saver(object):
             random.setstate(checkpoint['random_state'])
 
             return deformator, shift_predictor, deformator_opt, shift_predictor_opt , checkpoint['step']
-        elif algo == 'ours':
-            if params is None:
-                seed_value = checkpoint['seed']
-                generator_number = checkpoint['generator_number']
-                step = checkpoint['step']
-                return seed_value, generator_number, step
-            else:
-                deformator, deformator_opt, cr_discriminator, cr_optimizer = params
-                deformator.load_state_dict(checkpoint['deformator'])
-                deformator_opt.load_state_dict(checkpoint['deformator_opt'])
-                cr_discriminator.load_state_dict(checkpoint['cr_discriminator'])
-                cr_optimizer.load_state_dict(checkpoint['cr_optimizer'])
-                torch.set_rng_state(checkpoint['torch_rng_state'])
-                np.random.set_state(checkpoint['np_rng_state'])
-                random.setstate(checkpoint['random_state'])
-                seed_value = checkpoint['seed']
-                generator_number = checkpoint['generator_number']
-                step = checkpoint['step']
-                return deformator, deformator_opt, cr_discriminator, cr_optimizer,seed_value,generator_number,step
+        elif algo == 'ours-natural':
+            deformator, cr_discriminator, deformator_opt, cr_optimizer = params
+            deformator.load_state_dict(checkpoint['deformator'])
+            cr_discriminator.load_state_dict(checkpoint['cr_discriminator'])
+            deformator_opt.load_state_dict(checkpoint['deformator_opt'])
+            cr_optimizer.load_state_dict(checkpoint['cr_optimizer'])
+            torch.set_rng_state(checkpoint['torch_rng_state'])
+            np.random.set_state(checkpoint['np_rng_state'])
+            random.setstate(checkpoint['random_state'])
+            return deformator, cr_discriminator, deformator_opt, cr_optimizer
+
         else:
             raise NotImplementedError
 
