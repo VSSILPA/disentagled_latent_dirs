7483b3c0ebf2463d8678c194db91e275f492d822
adarsh <kadarsh22@gmail.com>
2021-08-15 18:47:43 +0530

    animes dataset ld ours

--- src/models/latentdiscovery/utils.py
+++ src/models/latentdiscovery/utils.py
@@ -2,6 +2,7 @@ import os
 import torch
 from .gan_load import make_big_gan, make_proggan, make_sngan
 from .latent_deformator import LatentDeformator
+from models.closedform.closedform_directions import CfLinear, CfOrtho
 
 GEN_CHECKPOINT_DIR = '../pretrained_models/generators/LatentDiscovery'
 DEFORMATOR_CHECKPOINT_DIR = '../pretrained_models/deformators/LatentDiscovery'
@@ -19,6 +20,7 @@ def load_generator(opt):
         from gan_load import make_style_gan2
         G = make_style_gan2(1024, G_weights, True)
     else:
+        G_weights = os.path.join(GEN_CHECKPOINT_DIR, model_name)
         G = make_sngan(G_weights)
 
     G.cuda().eval()
@@ -26,11 +28,8 @@ def load_generator(opt):
 
 def load_deformator(opt):
     model_name = opt.algo.ours.model_name
-    deformator = LatentDeformator(shift_dim=opt.algo.ours.latent_dim,
-                                  input_dim=opt.algo.ours.num_directions,
-                                  out_dim=opt.latent_dim,
-                                  type=opt.algo.ours.deformator_type,
-                                  random_init=True).cuda()
-    deformator.load_state_dict(torch.load(os.path.join(DEFORMATOR_CHECKPOINT_DIR, model_name, 'deformator_0.pt'), map_location=torch.device('cpu')))
+    directions = torch.load(os.path.join(DEFORMATOR_CHECKPOINT_DIR, model_name, 'deformator_0.pt'), map_location=torch.device('cpu'))['log_mat_half']
+    deformator = CfOrtho(opt.algo.ours.latent_dim, opt.algo.ours.num_directions)
+    deformator.ortho_mat.data = torch.FloatTensor(directions)
     deformator.cuda()
     return deformator
